500049d15a991d99008e7bea3799bf2fd0d039ac
Håkan Nylén <confact@users.noreply.github.com>
2016-05-11 19:59:15 +0200

    ugly fix to remove e rror for now.

--- lib/semantic/ui/sass/engine.rb
+++ lib/semantic/ui/sass/engine.rb
@@ -4,9 +4,9 @@ module Semantic
     module Sass
       module Rails
         class Engine < ::Rails::Engine
-          initializer "semantic-ui-sass.assets.precompile" do |app|
-            app.config.assets.precompile << %r(semantic-ui\/(basic\.)*icons\.(?:eot|svg|ttf|woff)$)
-          end
+          #initializer "semantic-ui-sass.assets.precompile" do |app|
+          #  app.config.assets.precompile << %r(semantic-ui\/(basic\.)*icons\.(?:eot|svg|ttf|woff)$)
+          #end
           initializer 'semantic-ui-sass.setup_helpers' do |app|
             app.config.to_prepare do
               ActionController::Base.send :include, Semantic::Ui::Sass::BreadCrumbs
